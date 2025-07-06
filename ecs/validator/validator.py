a120036325762dd95a6fd96f550f50fcbed5ce56
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-07-06 20:26:36 +0000

    Load the environment variables for the validator script

--- ecs/validator/validator.py
+++ ecs/validator/validator.py
@@ -5,3 +5,9 @@ import io
 import json
 from datetime import datetime
 from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+BUCKET = os.getenv("BUCKET")
+
+s3 = boto3.client("s3")
