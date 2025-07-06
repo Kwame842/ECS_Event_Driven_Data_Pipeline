70e8a2280e5d13887637970f48dc758edf898e52
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-07-06 20:33:16 +0000

    Initial Spark for the transformer script

--- ecs/transformer/transformer.py
+++ ecs/transformer/transformer.py
@@ -17,3 +17,17 @@ ORDER_KPI_TABLE = os.getenv("ORDER_KPI_TABLE")
 CATEGORY_KPI_TABLE = os.getenv("CATEGORY_KPI_TABLE")
 PROCESSED_TABLE = os.getenv("PROCESSED_TABLE")
 PRODUCTS_PREFIX = os.getenv("PRODUCTS_PREFIX", "static/")
+
+# Initialize Spark
+spark = (
+    SparkSession.builder.appName("ECS-Order-KPI")
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    .config(
+        "spark.hadoop.fs.s3a.aws.credentials.provider",
+        "com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
+    )
+    .config("spark.hadoop.fs.s3a.path.style.access", "true")
+    .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "true")
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com")
+    .getOrCreate()
+)
